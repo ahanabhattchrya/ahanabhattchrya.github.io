086913672926490f01409dbc158e62eceb7989c8
ahanabhattchrya <ahanabha@buffalo.edu>
2024-09-10 13:15:30 -0400

    Added the out file

---
+++ out/_next/static/chunks/app/contact/page-064cb15ea0274efe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{3542:function(n,t,e){Promise.resolve().then(e.t.bind(e,2859,23))},2859:function(n){n.exports={main:"contact_main__SQQdP",coffeeShops:"contact_coffeeShops__fCON3",text:"contact_text__cAgI3"}}},function(n){n.O(0,[436,971,23,744],function(){return n(n.s=3542)}),_N_E=n.O()}]);
